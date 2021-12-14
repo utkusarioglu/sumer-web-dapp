604e2b77a45e52f27f743b5d4aaae6bb5b4aa14f
Utku Sarioglu <utkusarioglu@hotmail.com>
2021-12-14 09:52:26 +0000

    Create startup module
    __startup.ts
    - Create a startup module for startup actions of the react app.
      This module will be short lived as these functions will be
      spread around as lazy loading is implemented

---
+++ src/__startup.ts
@@ -0,0 +1,19 @@
+// import io from "_services/io/io.service";
+import ethereumService from "_services/ethereum/ethereum.service";
+import { push } from "_/slices/blocks/blocks.slice";
+import fetchService from "_services/fetch/fetch.service";
+import { setUser } from "_slices/user/user.slice";
+
+// export function startIo() {
+//   console.log(io);
+// }
+
+export function startEthereum() {
+  ethereumService.listen({
+    newBlockCallback: (block) => push(JSON.parse(JSON.stringify(block))),
+  });
+}
+
+export function startUser() {
+  fetchService.fetchUser((data) => setUser(data));
+}
