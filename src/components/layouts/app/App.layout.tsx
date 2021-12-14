e1cb0847dcb90e872b6aa43b2fb39840f01f97e9
Utku Sarioglu <utkusarioglu@hotmail.com>
2021-12-14 10:03:41 +0000

    Create BottomNavigation.view
    BottomNavigation.view.tsx
    - Create component to serve as the bottom navigation
    
    App.layout.tsx
    - Add BottomNavigationView into the layout

--- src/components/layouts/app/App.layout.tsx
+++ src/components/layouts/app/App.layout.tsx
@@ -6,6 +6,7 @@ const AppLayout = () => {
   return (
     <>
       <AppBarView />
+      <BottomNavigationView />
     </>
   );
 };
---
+++ src/components/views/bottom-navigation/BottomNavigation.view.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import Paper from "@mui/material/Paper";
+import Inventory2Icon from "@mui/icons-material/Inventory2";
+import ReceiptLongIcon from "@mui/icons-material/ReceiptLong";
+import ReceiptIcon from "@mui/icons-material/Receipt";
+
+const BottomNavigationView = () => {
+  const [value, setValue] = useState(0);
+
+  return (
+    <Paper
+      sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+      elevation={3}
+    >
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(_event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        <BottomNavigationAction label="Blocks" icon={<Inventory2Icon />} />
+        <BottomNavigationAction
+          label="Transactions"
+          icon={<ReceiptLongIcon />}
+        />
+        <BottomNavigationAction label="Events" icon={<ReceiptIcon />} />
+      </BottomNavigation>
+    </Paper>
+  );
+};
+
+export default BottomNavigationView;
