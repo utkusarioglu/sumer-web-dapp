b52b5a1d2ef6f5e3fb08a6f25c7b7ef24b6e225e
Utku Sarioglu <utkusarioglu@hotmail.com>
2021-12-14 09:54:31 +0000

    Create AppLayout component
    App.layout.tsx
    - Create component that will carry the entire layout of the app.

---
+++ src/components/layouts/app/App.layout.tsx
@@ -0,0 +1,12 @@
+import AppBarView from "_views/app-bar/AppBar.view";
+import BlockListView from "_views/block-list/BlockList.view";
+import BottomNavigationView from "_views/bottom-navigation/BottomNavigation.view";
+
+const AppLayout = () => {
+  return (
+    <>
+    </>
+  );
+};
+
+export default AppLayout;
