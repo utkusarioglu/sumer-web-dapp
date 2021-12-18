0aefd8107825f00845b8b8670bb717b24da10005
Utku Sarioglu <utkusarioglu@hotmail.com>
2021-12-18 15:24:34 +0000

    Create wasm.service
    - create `wasm.service`. This service is responsible for handling
      communications with the webassembly vm.

---
+++ src/services/wasm/wasm.service.ts
@@ -0,0 +1,23 @@
+export class WasmService {
+  private instance: any;
+
+  public async init() {
+    const module = await require("/usr/src/app/wasm/build/wasm.js");
+    const instance = await module.default();
+    this.instance = instance;
+  }
+
+  public async lerp(...values: number[]) {
+    const instance = await this.getInstance();
+    return instance.lerp(...values);
+  }
+
+  public async getInstance() {
+    if (!this.instance) {
+      await this.init();
+    }
+    return this.instance;
+  }
+}
+
+export default new WasmService();
