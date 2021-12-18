c54a280b0132e4219791c558bb4d43f00fb5cbd2
Utku Sarioglu <utkusarioglu@hotmail.com>
2021-12-18 15:27:38 +0000

    Add dummy AppDrawer component
    - Add AppDrawer.view for rendering the app drawer. This is a dummy
      component, meaning that its contents has not yet been customized
      for the app.

---
+++ src/components/views/app-drawer/AppDrawer.view.tsx
@@ -0,0 +1,45 @@
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+import { useSelector } from "react-redux";
+import { selectAppDrawerOpen, setAppDrawerOpen } from "_/slices/app/app.slice";
+
+function AppDrawerView() {
+  const state = useSelector(selectAppDrawerOpen);
+
+  return (
+    <Drawer anchor="left" open={state} onClose={() => setAppDrawerOpen(false)}>
+      <Box sx={{ width: 250 }}>
+        <List>
+          {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {["All mail", "Trash", "Spam"].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+    </Drawer>
+  );
+}
+
+export default AppDrawerView;
