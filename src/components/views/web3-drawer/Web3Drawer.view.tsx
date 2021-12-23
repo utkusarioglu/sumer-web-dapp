085dfe031017ac91c4b28f88a960f35c4a491b19
Utku Sarioglu <utkusarioglu@hotmail.com>
2021-12-23 13:27:15 +0000

    Create Web3Drawer
    Web3ConnectOptions.view.tsx
    - Create a component to contain web3 connection options
    
    Web3Drawer.view.tsx
    - Create a drawer component to handle web3 connectivity choices
      by the user
    - Add Web3ConnectOptionsView as a component
    
    App.layout.tsx
    - Add Web3DrawerView to the layout

---
+++ src/components/views/web3-drawer/Web3Drawer.view.tsx
@@ -0,0 +1,59 @@
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+import Container from "@mui/material/Container";
+import { useSelector } from "react-redux";
+import {
+  selectWeb3DrawerOpen,
+  setUserDrawerOpen,
+} from "_/slices/app/app.slice";
+import { selectUserProfile, selectAccounts } from "_slices/user/user.slice";
+import { Typography } from "@mui/material";
+import Web3ConnectOptionsView from "../web3-connect-options/Web3ConnectOptions.view";
+
+function Web3DrawerView() {
+  const state = useSelector(selectWeb3DrawerOpen);
+  const userProfile = useSelector(selectUserProfile);
+  const accounts = useSelector(selectAccounts);
+
+  if (!userProfile) {
+    return null;
+  }
+
+  return (
+    <Drawer
+      anchor="bottom"
+      open={state}
+      onClose={() => setUserDrawerOpen(false)}
+    >
+      <Box>
+        <Container>
+          <Typography variant="h6" align="center">
+            Connect to your wallet
+          </Typography>
+        </Container>
+        {accounts ? (
+          <Container>
+            {accounts.map((account) => (
+              <Typography key={account}>
+                You are connected through: {account.slice(0, 7)}
+              </Typography>
+            ))}
+          </Container>
+        ) : (
+          <Container>
+            <Web3ConnectOptionsView />
+          </Container>
+        )}
+      </Box>
+    </Drawer>
+  );
+}
+
+export default Web3DrawerView;
