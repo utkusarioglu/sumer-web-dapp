42de9d862a179344c93f1073613f7e4fe5fb8fac
Utku Sarioglu <utkusarioglu@hotmail.com>
2021-12-30 17:29:25 +0300

    Update startup
    __startup.ts
    - Enable socketio service to access the websocket backend

--- src/__startup.ts
+++ src/__startup.ts
@@ -1,12 +1,12 @@
-// import io from "_services/io/io.service";
+import io from "_services/io/io.service";
 import ethereumService from "_services/ethereum/ethereum.service";
 import { push } from "_/slices/blocks/blocks.slice";
 import fetchService from "_services/fetch/fetch.service";
 import { setUserProfile } from "_slices/user/user.slice";
 
-// export function startIo() {
-//   console.log(io);
-// }
+export function startIo() {
+  console.log(io);
+}
 
 export function startEthereum() {
   ethereumService.listen({
